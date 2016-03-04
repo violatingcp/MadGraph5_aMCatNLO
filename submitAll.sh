6bf964a6e51e397825e6dcc5134c1bce36cf8aa0
Philip <violatingcp@gmail.com>
2016-03-04 02:33:50 +0100

    made example gridpack setup

---
+++ submitAll.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+#example sumbit
+./install.py --carddir Cards/VectorMonoWLV/ --name VectorMonoWLV  --resubmit True
+./install.py --carddir Cards/VectorMonoZLL/ --name VectorMonoZLL  --resubmit True
+./install.py --carddir Cards/AxialMonoWLV/  --name AxialMonoWLV   --resubmit True
+./install.py --carddir Cards/AxialMonoZLL/  --name AxialMonoZLL   --resubmit True
+./install.py --carddir Cards/AxialMonoW/  --name AxialMonoW   --dmr#ange 100 --medrange 100 --resubmit True
+
+
+ 
